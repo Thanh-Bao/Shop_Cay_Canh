61c8b82005d7b55fd4649e28270ef1a300b0b272
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2021-04-04 15:50:47 +0700

    Change router

--- BonsaiShop/ClientApp/src/App.js
+++ BonsaiShop/ClientApp/src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
-
-import CustomerRouter from './components/Router/CustomerRouter';
-import AdminRouter from './components/Router/AdminRouter'
-import Unauthorized from './components/Utility/Unauthorized';
-
-
+import AppRouter from './router/AppRouter';
 
 class App extends Component {
     render() {
         return (
-            <Router>
-                <Route exact path="/KhongCoQuyenTruyCap" component={Unauthorized} />
-                <CustomerRouter />
-                <AdminRouter />
-            </Router>
+            <AppRouter/>
         );
     }
 }
---
+++ BonsaiShop/ClientApp/src/router/AppRouter.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Layout
+import AdminLayout from '../layout/Admin';
+import CustomerLayout from '../layout/Customer';
+
+import Home from '../pages/customer/Home';
+import ProductDetail from '../pages/customer/ProductDetail';
+
+import Dashboard from '../pages/admin/Dashboard';
+import UsesManagement from '../pages/admin/UsersManagement';
+
+import PageNotFound from '../pages/utility/PageNotFound';
+import ProtectedRoute from '../router/ProtectedRoute';
+import Unauthorized from '../pages/utility/Unauthorized';
+
+
+class AppRouter extends Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+
+                    <Route exact path="/unauthorized" component={Unauthorized} />
+
+                    <Route exact path="/admin/:path?">
+                        <AdminLayout>
+                            <Switch>
+                                <ProtectedRoute path="/admin" component={Dashboard} isAuth={false} />
+                                <ProtectedRoute exact path="/admin/users" component={UsesManagement} isAuth={true}/>
+                                <Route path="*" component={PageNotFound} />
+                            </Switch>
+                        </AdminLayout>
+                    </Route>
+
+                    <Route>
+                        <CustomerLayout>
+                            <Switch>
+                                <Route exact path="/" component={Home} />
+                                <Route exact path="/productdetail" component={ProductDetail} />
+                                <Route path="*" component={PageNotFound} />
+                            </Switch>
+                        </CustomerLayout>
+                    </Route>             
+
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+export default AppRouter;
