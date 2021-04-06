57d0eb6b450d3319266e34c2fa4c1139d1afe663
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2021-04-06 16:51:16 +0700

    Finish cart

--- BonsaiShop/ClientApp/src/components/CartProduct.js
+++ BonsaiShop/ClientApp/src/components/CartProduct.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import TextTruncate from 'react-text-truncate';
 
 class CartProduct extends Component {
     render() {
+        var numeral = require('numeral');
         return (
             <div className="card h-100">
                 <a className="card-images-change wraper-fixed-img" href="#">
@@ -9,10 +11,27 @@ class CartProduct extends Component {
                     <img className="card-img-top img-top-change img-item-fixed" src={this.props.fullImage} alt="Back" />
                 </a>
                 <div className="card-body">
-                    <h4 className="card-title">
-                        <a href="#">Project Three</a>
-                    </h4>
-                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quisquam, error quod sed cumque, odio distinctio velit nostrum temporibus necessitatibus et facere atque iure perspiciatis mollitia recusandae vero vel quam!</p>
+                    <div className="text-center">
+                        <h3 className="card-title font-weight-bold text-truncate">
+                            <a className="text-decoration-none" href="#">Project Three </a>
+                        </h3>
+                        <h3 className=" font-weight-bold text-danger" >{numeral(800000).format('0,0')} đ
+                        </h3>
+                    </div>
+
+                    <hr />
+                    <TextTruncate
+                        line={3}
+                        element="p"
+                        truncateText=" …"
+                        containerClassName="card-text text-card-description"
+                        text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quisquam, error quod sed cumque, odio distinctio velit nostrum temporibus necessitatibus et facere atque iure perspiciatis mollitia recusandae vero vel quam!"
+                    />
+
+                    <div class="d-flex justify-content-between">
+                        <button type="button" class="btn-cart-product btn btn-outline-primary"><i class="fas fa-eye"></i>Xem chi tiết</button>
+                        <button type="button" class="btn-cart-product btn btn-primary"><i class="fas fa-cart-plus"></i> Thêm vào giỏ hàng</button>
+                    </div>
                 </div>
             </div>
         );
