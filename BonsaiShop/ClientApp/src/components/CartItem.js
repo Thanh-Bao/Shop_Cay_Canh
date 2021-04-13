4cc311069ecfc62cbf42db34691e936568ea6b4c
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2021-04-13 17:06:26 +0700

    sạgijs

--- BonsaiShop/ClientApp/src/components/CartItem.js
+++ BonsaiShop/ClientApp/src/components/CartItem.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react';
+import '../css/ItemCart.css';
+import CallAPI from '../callAPI/callAPIMainServer';
+import { connect } from 'react-redux';
 
 class CartItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
             quantity: this.props.quantity,
-            productID : this.props.productID
-        }
+            productID: this.props.productID
+        };
     }
+
+    handleClick(productID, minus) {
+        let userPhone = localStorage.getItem("PHONEUSERLOGINED");
+        if (minus) {
+            if (this.state.quantity > 1) {
+                this.setState({
+                    quantity: this.state.quantity - 1
+                })
+                CallAPI(`Cart/${userPhone}`, 'PATCH', { productID: productID }).catch(() => {
+                    alert("Lỗi Thêm sản phẩm vảo giỏ hàng");
+                })
+            }
+        } else {
+            this.setState({
+                quantity: this.state.quantity + 1
+            })
+            CallAPI(`Cart/${userPhone}`, 'PUT', { productID: productID }).catch(() => {
+                alert("Lỗi Thêm sản phẩm vảo giỏ hàng");
+            })
+        }
+
+        CallAPI(`Cart/count/${userPhone}`).then(
+            res => {
+                this.props.dispatch({ type: "UPDATE_TOTAL_ITEM_CART", data: res.data });
+            }).catch(() => {
+                alert("Lỗi lấy tổng sản phẩm giỏ hàng");
+            })
+    }
+
+
+    deleteItem(productID) {
+        let userPhone = localStorage.getItem("PHONEUSERLOGINED");
+        CallAPI(`Cart/${userPhone}`, 'DELETE', { productID: productID }).catch(() => {
+            alert("Lỗi Xóa sản phẩm");
+        });
+
+        CallAPI(`Cart/count/${userPhone}`).then(
+            res => {
+                this.props.dispatch({ type: "UPDATE_TOTAL_ITEM_CART", data: res.data });
+            }).catch(() => {
+                alert("Lỗi lấy tổng sản phẩm giỏ hàng");
+            });
+    }
+
+
     render() {
         var numeral = require('numeral');
         return (
@@ -16,11 +64,29 @@ class CartItem extends Component {
                     <img src={this.props.thumbnail} width={100} alt="fdf" />
                     <strong className="ml-5 fw-bold">{this.props.name}</strong>
                 </td>
-                <td><span className="ml-5">Đơn giá: <strong className=" text-danger">{numeral(this.props.price).format('0,0')}đ</strong> </span></td>
-                <td><h1 className="ml-5">{this.state.quantity} xxxx {this.props.productID}</h1></td>
+                <td><span className="ml-5">Đơn giá: <strong className=" text-danger">{numeral(this.props.price).format('0,0')}đ</strong> </span> X </td>
+                <td>
+                    <div className="row">
+                        <button onClick={() => this.handleClick(this.props.productID, true)} name="minus" id="MinusItemCart" type="button" className="btn btn-light"><i className="fas fa-minus"></i></button>
+                        <input size={1}
+                            min={1}
+                            max={9999}
+                            value={this.state.quantity} disabled
+                            className="fw-bold" />
+                        <button onClick={() => this.handleClick(this.props.productID, false)} name="plus" id="PlusItemCart" type="button" className="btn btn-light"><i className="fas fa-plus"></i></button>
+                    </div>
+                </td>
+                <td className=" text-danger">
+                    =  {numeral(this.props.price * this.state.quantity).format('0,0')} đ
+                </td>
+                <td>
+                    <button onClick={() => this.deleteItem(this.props.productID)} type="button" className=" ml-5 btn btn-secondary"><i className="fas fa-trash-alt"></i> xóa</button>
+                </td>
             </tr>
         );
     }
 }
 
-export default CartItem;
+const mapStateToProps = state => ({
+});
+export default connect(mapStateToProps)(CartItem);
