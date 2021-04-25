0bc8b64fe0424155be18977a1fa85e125c8e08d5
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2021-04-25 21:03:12 +0700

    fix router url product detail

--- BonsaiShop/ClientApp/src/components/CardProduct.js
+++ BonsaiShop/ClientApp/src/components/CardProduct.js
@@ -42,16 +42,16 @@ class CardProduct extends Component {
 
         return (
             <div className="card h-100">
-                <Link className="card-images-change wraper-fixed-img container-img-overlay" to={`/product-detail/` + this.props.productID}>
+                <a className="card-images-change wraper-fixed-img container-img-overlay" href={`/product-detail/` + this.props.productID}>
                     <img  className=" card-img-top img-item-fixed" src={this.props.thumbnail} alt="Front" />
                     <img className="card-img-top img-top-change img-item-fixed" src={this.props.fullImage} alt="Back" />
                     <span className="bottom-left-img-overlay mr-2 badge badge-pill badge-primary d-inline">Cao: {this.props.height} cm</span>
                     <span className="bottom-right-img-overlay mr-2 badge badge-pill badge-info d-inline">Xuất xứ: {this.props.origin}</span>
-                </Link>
+                </a>
                 <div className="card-body">
                     <div className="text-center">
                         <h3 className="card-title font-weight-bold text-truncate">
-                            <Link className="text-decoration-none" to={`/product-detail/` + this.props.productID}>{this.props.name}</Link>
+                            <a className="text-decoration-none" href={`/product-detail/` + this.props.productID}>{this.props.name}</a>
                         </h3>
                         <h3 className=" font-weight-bold text-danger" >{numeral(this.props.price).format('0,0')} đ
                         </h3>
@@ -67,7 +67,7 @@ class CardProduct extends Component {
                     />
 
                     <div className="d-flex mt-2 justify-content-between">
-                        <Link to={`/product-detail/` + this.props.productID}>  <button type="button" className="btn-cart-product btn btn-outline-primary"><i className="fas fa-eye"></i>Xem chi tiết</button></Link>
+                        <a href={`/product-detail/` + this.props.productID}>  <button type="button" className="btn-cart-product btn btn-outline-primary"><i className="fas fa-eye"></i>Xem chi tiết</button></a>
                         <button onClick={() => { addCart(this.props.productID) }} type="button" className="btn-cart-product btn btn-primary"><i className="fas fa-cart-plus"></i> Thêm vào giỏ hàng</button>
                     </div>
                 </div>
